205e9b267a497a431230e9ea53dc77e96494cbcb
cgruber <cgruber@google.com>
2015-03-02 10:33:08 -0800

    Replace exec of srcrr (sourcrer command-line) with []3 calls to piper-api.  This purely pulls out srcrr, and does not entirely strip from the code design elements which may remain that were in service of srcrr, such as distinct repository types for []/googlemac/googleclient, etc. 	Change on 2015/03/02 by cgruber <cgruber@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=87527226

--- java/com/google/devtools/moe/client/MoeProblem.java
+++ java/com/google/devtools/moe/client/MoeProblem.java
@@ -9,14 +9,19 @@ package com.google.devtools.moe.client;
  * @author dbentley@google.com (Daniel Bentley)
  */
 public class MoeProblem extends RuntimeException {
+  // https://www.youtube.com/watch?v=xZ4tNmnuMgQ
 
+  // TODO(cgruber): Figure out why this is public mutable and fix.
   public String explanation;
+  private final Object[] args;
 
-  public MoeProblem(String explanation) {
+  // TODO(cgruber): Check not null and ensure no one is calling it that way.
+  public MoeProblem(String explanation, Object ... args) {
     this.explanation = explanation;
+    this.args = args;
   }
 
   @Override
-  public String getMessage() { return explanation; }
+  public String getMessage() { return String.format(explanation, args); }
 
 }
