00a32a63a2d0061bd409aae38494587315a35a37
Christian Edward Gruber <cgruber@google.com>
2015-12-10 23:19:20 -0800

    Tweak the interim files generated and error messages.

--- util/make-binary.sh
+++ util/make-binary.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -x
+
 cmd_dir=$(cd `dirname "$0"` && pwd)
 target_dir="$cmd_dir/../target"
-jar_file=${target_dir}/moe-*-shaded.jar
+jar_file=${target_dir}/moe-*-executable.jar
 if [ ! -f ${jar_file} ]; then
-  echo "Shaded jar not found in ${target_dir}"
+  echo "Monolithic executable jar not found in ${target_dir}"
   exit 1
 fi
 cat ${cmd_dir}/java-exec-prefix ${jar_file} > ${target_dir}/moe
 chmod a+x ${target_dir}/moe
+echo "MOE Mac/Unix excecutable created at ${target_dir}/moe"
