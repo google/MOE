2c7c5e381851894c9ad80013970968c918648775
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2016-02-15 03:24:22 -0600

    Refactoring the MeoProblem class. For this refactoring, the class documentation was updated and one comment for a video from youtube was removed.

--- java/com/google/devtools/moe/client/MoeProblem.java
+++ java/com/google/devtools/moe/client/MoeProblem.java
@@ -17,11 +17,10 @@
 package com.google.devtools.moe.client;
 
 /**
- * A problem that we do not expect to routinely happen. They should end execution of MOE and require
- * intervention by moe-team.
+ * A problem that we do not expect to routinely happen. They should end 
+ * execution of MOE and require intervention by moe-team.
  */
 public class MoeProblem extends RuntimeException {
-  // https://www.youtube.com/watch?v=xZ4tNmnuMgQ
 
   // TODO(cgruber): Figure out why this is public mutable and fix.
   public String explanation;
